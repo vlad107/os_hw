08afb534ed94015d1d04b7532192beff148538f5
Vlad <vlad10795@gmail.com>
2016-04-02 20:20:03 +0300

    hello_world&cat homeworks done.

---
+++ cat/cat2.c
@@ -0,0 +1,24 @@
+int main() {
+	const int BUFF_SIZE = 2048;
+	const char* ERR_WRITE = "An error occured while writing\n";
+	const int ERR_WRITE_LEN = 31;
+	const char* ERR_READ = "An error occured while reading\n";
+	const int ERR_READ_LEN = 31;
+
+	char buffer[BUFF_SIZE];
+	int _read = 0;
+	while ((_read = read(0, buffer, BUFF_SIZE)) > 0) {
+		int cur = 0;
+		while (cur < _read) {
+			int _write = write(1, buffer + cur, _read - cur);
+			if (_write < 0) {
+				write(2, ERR_WRITE, ERR_WRITE_LEN);
+				return 1;
+			}
+			cur += _write;
+		}
+	}
+	if (_read < 0) {
+		write(2, ERR_READ, ERR_READ_LEN);
+	}
+}
